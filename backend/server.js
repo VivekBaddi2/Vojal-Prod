bc569cb9c779ef83790661e940fd020b871ae711
Amrut-git011 <mytuf15@gmail.com>
2026-01-07 13:45:52 +0530

    setup flowtec

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongooseConnection from "./mongo.js";
+import appRoutes from "./routes/index.js";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+// Middleware
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// DB Connection
+mongooseConnection();
+
+// CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Health Check
+app.get("/health", (req, res) => {
+  return res.status(200).json({
+    msg: "Server is up and running",
+  });
+});
+
+// Routes
+app.use("/api", appRoutes);
+
+// Start Server — works for both local & Render
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
